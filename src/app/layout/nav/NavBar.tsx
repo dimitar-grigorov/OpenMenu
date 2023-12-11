30fd44be36f050301267ff509dce6ce3150b7902
dimitar-grigorov <mitak.backtrack@gmail.com>
2023-12-11 18:53:13 +0200

    Added Login/Logout buttons and Menu for signed in user.

--- src/app/layout/nav/NavBar.tsx
+++ src/app/layout/nav/NavBar.tsx
@@ -1,7 +1,12 @@
 import { NavLink } from 'react-router-dom';
 import { Container, Menu, MenuItem } from 'semantic-ui-react';
+import { useAppSelector } from '../../store/store';
+import SignedInMenu from './SignedInMenu';
+import SignedOutButtons from './SignedOutButtons';
 
 export default function NavBar() {
+    const { authenticated } = useAppSelector(state => state.auth)
+
     return (
         <Menu inverted={true} fixed='top'>
             <Container>
@@ -11,6 +16,8 @@ export default function NavBar() {
                 </MenuItem>
                 <MenuItem name='Menu' as={NavLink} to='/menu' />
                 <MenuItem name='Lab' as={NavLink} to='/lab' />
+
+                {authenticated ? <SignedInMenu /> : <SignedOutButtons />}
             </Container>
         </Menu>
     )
---
+++ src/app/layout/nav/SignedInMenu.tsx
@@ -0,0 +1,28 @@
+import { useNavigate } from 'react-router-dom';
+import { Menu, Image, Dropdown } from 'semantic-ui-react';
+import { useAppDispatch, useAppSelector } from '../../store/store';
+import { signOut } from '../../../features/auth/authSlice';
+
+export default function SignedInMenu() {
+    const { currentUser } = useAppSelector(state => state.auth);
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+
+    function handleSignOut() {
+        dispatch(signOut());
+        navigate('/');
+    }
+
+    return (
+        <Menu.Item position='right'>
+            <Image avatar spaced='right' src='/user.png' />
+            <Dropdown pointing='top left' text={currentUser?.email}>
+                <Dropdown.Menu>
+                    {/* <Dropdown.Item as={Link} to='/createMenuItem' text='Create Menu Item' icon='plus' /> */}
+                    <Dropdown.Item text='My profile' icon='user' />
+                    <Dropdown.Item onClick={handleSignOut} text='Sign out' icon='power' />
+                </Dropdown.Menu>
+            </Dropdown>
+        </Menu.Item>
+    )
+}
