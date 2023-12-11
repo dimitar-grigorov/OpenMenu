aeb84d8f22a4b4f8f46fc19cd4eaea6f794e0238
dimitar-grigorov <mitak.backtrack@gmail.com>
2023-12-11 20:18:47 +0200

    Added ModalManager component which is used to display different kind of modals

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,11 +1,13 @@
 import { Container } from "semantic-ui-react"
 import NavBar from "./nav/NavBar"
 import { Outlet } from "react-router-dom"
+import ModalManager from "../common/modals/ModalManager"
 
 function App() {
 
   return (
     <>
+      <ModalManager />
       <NavBar />
       <Container className="main">
         <Outlet />
