1905c0d4fd89d10edfcbd6346db67daeba742b75
dimitar-grigorov <mitak.backtrack@gmail.com>
2023-12-13 00:03:36 +0200

    Fixed sign out

--- src/app/layout/nav/SignedInMenu.tsx
+++ src/app/layout/nav/SignedInMenu.tsx
@@ -1,25 +1,24 @@
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Menu, Image, Dropdown } from 'semantic-ui-react';
-import { useAppDispatch, useAppSelector } from '../../store/store';
-import { logout } from '../../../features/auth/authSlice';
+import { useAppSelector } from '../../store/store';
+import { signOut } from 'firebase/auth';
+import { auth } from '../../config/firebase';
 
 export default function SignedInMenu() {
     const { currentUser } = useAppSelector(state => state.auth);
-    const dispatch = useAppDispatch();
     const navigate = useNavigate();
 
-    function handleSignOut() {
-        dispatch(logout());
+    async function handleSignOut() {
+        await signOut(auth)
         navigate('/');
     }
 
     return (
         <Menu.Item position='right'>
-            <Image avatar spaced='right' src='/user.png' />
-            <Dropdown pointing='top left' text={currentUser?.email}>
+            <Image avatar spaced='right' src={currentUser?.photoURL || '/user.png'} />
+            <Dropdown pointing='top left' text={currentUser?.displayName as string}>
                 <Dropdown.Menu>
-                    {/* <Dropdown.Item as={Link} to='/createMenuItem' text='Create Menu Item' icon='plus' /> */}
-                    <Dropdown.Item text='My profile' icon='user' />
+                    <Dropdown.Item as={Link} to='/account' text='My account' icon='settings' />
                     <Dropdown.Item onClick={handleSignOut} text='Sign out' icon='power' />
                 </Dropdown.Menu>
             </Dropdown>
